780ee84871d68fd58ac9fd956efeef1f63bb6743
Valeriana_C <2844883484@qq.com>
2026-03-05 20:56:19 +0800

    alpha

---
+++ TagList.js
@@ -0,0 +1,16 @@
+// Full tag list, from INT wiki
+// Most recently updated at 2025.8.--
+const FullTagList =
+{
+    "admin" : "管理",
+    "artist" : "艺术家",
+    "artwork" : "艺术作品",
+    "author" : "作者",
+    "component" : "×é¼þ",
+    "component-backend" : "×é¼þºó¶Ë",
+    "essay" : "ÎÄÕÂ",
+    "fragment": "¶ÎÂä",
+
+    "scp" : "scp",
+    
+}
